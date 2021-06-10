c5dbb8e3dee3aee07673da6cb3da6725f726591e
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2021-06-10 09:31:43 -0500

    removed unneeded

--- update_vcpkg.ps1
+++
@@ -1,6 +0,0 @@
-Push-Location $PSScriptRoot
-Get-ChildItem ..\build\buildtrees\ | 
-    Select-Object -expand name | ForEach-Object { 
-        Copy-Item "..\thirdparty\vcpkg\ports\$_" -Destination . -Recurse -Force
-}
-Pop-Location
