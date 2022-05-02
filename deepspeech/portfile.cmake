35de9dce399ba9afe3975c528eb41b6b1574ada2
luiz <luiz@ohanar.com>
2022-05-02 18:22:36 -0400

    added cmake port library

---
+++ deepspeech/portfile.cmake
@@ -0,0 +1,25 @@
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO luiz-monad/deepspeech
+    REF customport
+    SHA512 @SHA512@
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup()
+
+vcpkg_copy_pdbs()
+
+file(
+    INSTALL ${CMAKE_CURRENT_LIST_DIR}/LICENSE
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+    RENAME copyright
+)
